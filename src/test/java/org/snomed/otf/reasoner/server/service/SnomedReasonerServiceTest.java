936e5031877e72723fe5b38c36afe7bb167c5229
Kai Kewley <kaikewley@gmail.com>
2017-11-07 00:26:21 +0000

    CLASS-15 Integration test for CGIs working + detailed comments.

--- src/test/java/org/snomed/otf/reasoner/server/service/SnomedReasonerServiceTest.java
+++ src/test/java/org/snomed/otf/reasoner/server/service/SnomedReasonerServiceTest.java
@@ -61,6 +61,51 @@ public class SnomedReasonerServiceTest {
 		assertTrue(lines.contains("1\t\t73211009\t113331007\t0\t" + FINDING_SITE + "\t900000000000011006\t900000000000451002"));
 	}
 
+	@Test
+	public void testClassifyGCI() throws IOException, OWLOntologyCreationException, ReleaseImportException, ReasonerServiceException {
+		File baseRF2SnapshotZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Base_snapshot");
+		File deltaZip = ZipUtil.zipDirectoryRemovingCommentsAndBlankLines("src/test/resources/SnomedCT_MiniRF2_Secondary_Diabetes_GCI_delta");
+		assertNotNull(snomedReasonerService);
+
+		// Run classification
+		File results = snomedReasonerService.classify(new FileInputStream(baseRF2SnapshotZip), new FileInputStream(deltaZip), REASONER_FACTORY_CLASS_NAME);
+
+		// Assert results
+		List<String> lines = readLinesTrim(results);
+		assertEquals(9, lines.size());
+
+		assertTrue("Redundant relationship. Diabetes caused by drug - Is a - Diabetes mellitus",
+				lines.contains("200107001\t\t0\t\t100103001\t73211009\t0\t116680003\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Redundant relationship. Diabetes due to cystic fibrosis - Is a - Diabetes mellitus",
+				lines.contains("200107001\t\t0\t\t100103001\t73211009\t0\t116680003\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Inferred relationship. Diabetes caused by drug - Is a - Secondary diabetes mellitus",
+				lines.contains("1\t\t100103001\t8801005\t0\t116680003\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Inferred relationship. Diabetes due to cystic fibrosis - Is a - Secondary diabetes mellitus",
+				lines.contains("1\t\t100104001\t8801005\t0\t116680003\t900000000000011006\t900000000000451002"));
+
+		// "Diabetes caused by drug" and "Diabetes due to cystic fibrosis" are classified as a type of "Secondary diabetes mellitus"
+		// This proves that the CGI axioms from the OWL Axiom reference set are working.
+
+
+
+		// There are some other inferences too:
+		assertTrue("Inferred relationship. Diabetes mellitus - Finding site - Structure of endocrine system",
+				lines.contains("1\t\t73211009\t113331007\t0\t363698007\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Inferred relationship. Secondary diabetes mellitus - Finding site - Structure of endocrine system",
+				lines.contains("1\t\t8801005\t113331007\t0\t363698007\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Inferred relationship. Diabetes caused by drug - Finding site - Structure of endocrine system",
+				lines.contains("1\t\t100103001\t113331007\t0\t363698007\t900000000000011006\t900000000000451002"));
+
+		assertTrue("Inferred relationship. Diabetes due to cystic fibrosis - Finding site - Structure of endocrine system",
+				lines.contains("1\t\t100104001\t113331007\t0\t363698007\t900000000000011006\t900000000000451002"));
+
+	}
+
 	private List<String> readLinesTrim(File results) throws IOException {
 		List<String> lines = new ArrayList<>();
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(ZipUtil.getZipEntryStreamOrThrow(results, "sct2_Relationship_Delta_Classification_")))) {
