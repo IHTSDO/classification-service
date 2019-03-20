4e1a23fb8deeab6cb238dde37e9184a6fa1b9ea6
Kai Kewley <kaikewley@gmail.com>
2019-03-20 22:08:39 +0000

    PIP-65 Add /version endpoint to report package version deployed.

---
+++ src/main/java/org/snomed/otf/reasoner/server/rest/VersionController.java
@@ -0,0 +1,27 @@
+package org.snomed.otf.reasoner.server.rest;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.snomed.otf.reasoner.server.pojo.BuildVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "Version", description = "Build Version")
+public class VersionController {
+
+	@Autowired
+	BuildProperties buildProperties;
+
+	@ApiOperation("Software build version and build timestamp.")
+	@RequestMapping(value = "/version", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public BuildVersion getBuildInformation() {
+		return new BuildVersion(buildProperties.getVersion(), buildProperties.getTime().toString());
+	}
+
+}
