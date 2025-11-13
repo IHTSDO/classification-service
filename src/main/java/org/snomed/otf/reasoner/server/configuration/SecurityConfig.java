53e97b8be744b80cbb67df1e06a18c7b1c93e5b8
Quyen Ly <quocquyenit@gmail.com>
2025-11-13 15:00:28 +0700

    PIP-908 Tweak security config after switching to new spring-boot version 3.5.7

--- src/main/java/org/snomed/otf/reasoner/server/configuration/SecurityConfig.java
+++ src/main/java/org/snomed/otf/reasoner/server/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +15,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.csrf().disable();
-		http.httpBasic();
+		http.csrf(AbstractHttpConfigurer::disable);
+		http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		return http.build();
 	}
 }
